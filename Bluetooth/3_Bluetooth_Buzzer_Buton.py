61578b7b8cc4d2035f387d5ae00207c1339a71a2
cogullu <64997079+cogullu@users.noreply.github.com>
2021-07-14 18:08:14 +0300

    Add files via upload

---
+++ Bluetooth/3_Bluetooth_Buzzer_Buton.py
@@ -0,0 +1,32 @@
+# Bluetooth Uygulaması #                     #// Bluetooth ile Pico bağlantısı uygulamasıdır.//#
+# Bluetooth çalıştırmak için Bluetooth jumperın takılı olduğundan emin olun.
+
+#Bluetooth kullanmak için önce mobil cihazınız ile DeneySeti üzerindeki HC-05 bluetooth modülünü eşleştirmeniz gerekir.
+#// daha sonra ise mobil cihazınıza DeneySetini ve Picoyu kontrol edebilecek bir uygulama kurmanız gerekir.
+#// Bunun için PlayStore mağazasındaki "Bluetooth Electronics" (keuwlsoft) uygulamasını kullanabilirsiniz.
+#// Uygulamadan kendiniz arayüz tasarlayabileceğiniz gibi, hazır arayüzü indirmek için de uygulamadan "Load From Web Link" bağlantısını kullanabilirsiniz.
+#// Hazır arayüz linki "https://raw.githubusercontent.com/cogullu/RPPicoDS/main/RPPicoDS_Bluetooth.kwl?token=APP4NV3A7USEOAW3TGE53FTA4IEGC"
+
+from machine import Pin, UART
+import utime
+
+uart = UART(0, 9600)           # 0 numaralı UART protokolü üzerinde 9600 hızında veri iletişimi
+buzzer = Pin(10, Pin.OUT)      # Buzzer Pin
+
+while True:
+    try:
+
+        if uart.any() > 0:            # Bluetooth ile Picoya bağlı cihazdan herhangi bir veri gelirse
+            data = uart.readline()    # Gelen 1 satırlık veriyi data isimli değişkene kaydet
+            
+# # # # # # ---------------------------------------Reset Butonu-----------------------------------------------------------------   
+            if 'Q' in data:           # Gelen veride Q karakteri varsa, "Ses Çal" Butonu
+                buzzer.value(1)
+                uart.write("*S*")     # Buzzer
+                
+                utime.sleep(2)
+                buzzer.value(0)
+    except:
+        print("Hay Aksi Birşeyler Yanlış Gitti!")
+        
+
